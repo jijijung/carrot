2fb12c96e9c287265bff5df27db80fa2a8e3a1a6
jijijung <87470236+jijijung@users.noreply.github.com>
2023-03-22 17:04:01 +0900

    JS: add sounds

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 'use strict';
 
 const CARROT_SIZE = 80;
-const CARROT_COUNT = 5;
-const BUG_COUNT = 5;
-const GAME_DURATION_SEC = 5;
+const CARROT_COUNT = 20;
+const BUG_COUNT = 20;
+const GAME_DURATION_SEC = 20;
 
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
 const gameBtn = document.querySelector('.game__button');
-const gameTimer = document.querySelector('.game__timer');
+const timerIndicator = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
 
 const popUp = document.querySelector('.pop-up');
 const popUpText = document.querySelector('.pop-up__message');
 const popUpRefresh = document.querySelector('.pop-up__refresh');
+
+const carrotSound = new Audio('./sound/carrot_pull.mp3');
+const alertSound = new Audio('./sound/alert.wav');
+const bgSound = new Audio('./sound/bg.mp3');
+const bugSound = new Audio('./sound/bug_pull.mp3');
+const winSound = new Audio('./sound/game_win.mp3');
 
 let started = false;
 let score = 0;
@@ -27,7 +33,6 @@ gameBtn.addEventListener('click', () => {
     startGame();
   }
 });
-
 popUpRefresh.addEventListener('click', () => {
   startGame();
   hidePopUp();
@@ -39,25 +44,36 @@ function startGame() {
   showStopButton();
   showTimerAndScore();
   startGameTimer();
+  playSound(bgSound);
 }
 
 function stopGame() {
   started = false;
   stopGameTimer();
   hideGameButton();
-  showPopUpWithText('REPLAY?');
+  showPopUpWithText('REPLAY❓');
+  playSound(alertSound);
+  stopSound(bgSound);
 }
 
 function finishGame(win) {
   started = false;
   hideGameButton();
-  showPopUpWithText(win ? 'YOU WON' : 'YOU LOST');
+  if (win) {
+    playSound(winSound);
+  } else {
+    playSound(bugSound);
+  }
+  stopGameTimer();
+  stopSound(bgSound);
+  showPopUpWithText(win ? 'YOU WON 🎉' : 'YOU LOST 💩');
 }
 
 function showStopButton() {
   const icon = gameBtn.querySelector('.fas');
   icon.classList.add('fa-stop');
   icon.classList.remove('fa-play');
+  gameBtn.style.visibility = 'visible';
 }
 
 function hideGameButton() {
@@ -65,20 +81,20 @@ function hideGameButton() {
 }
 
 function showTimerAndScore() {
-  gameTimer.style.visibility = 'visible';
+  timerIndicator.style.visibility = 'visible';
   gameScore.style.visibility = 'visible';
 }
 
 function startGameTimer() {
   let remainingTimeSec = GAME_DURATION_SEC;
-  updateTimeText(remainingTimeSec);
+  updateTimerText(remainingTimeSec);
   timer = setInterval(() => {
     if (remainingTimeSec <= 0) {
       clearInterval(timer);
-      finishGame(CARROT_COUNT === score);
+      finishGame(score === CARROT_COUNT);
       return;
     }
-    updateTimeText(--remainingTimeSec);
+    updateTimerText(--remainingTimeSec);
   }, 1000);
 }
 
@@ -86,10 +102,10 @@ function stopGameTimer() {
   clearInterval(timer);
 }
 
-function updateTimeText(time) {
+function updateTimerText(time) {
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
-  gameTimer.innerText = `${minutes}:${seconds}`;
+  timerIndicator.innerHTML = `${minutes}:${seconds}`;
 }
 
 function showPopUpWithText(text) {
@@ -102,9 +118,10 @@ function hidePopUp() {
 }
 
 function initGame() {
+  score = 0;
   field.innerHTML = '';
-  gameScore.innerHTML = CARROT_COUNT;
-  // 벌레와 당근을 생성한 뒤 field에 추가해줌
+  gameScore.innerText = CARROT_COUNT;
+  // 벌레와 당근을 생성한뒤 field에 추가해줌
   addItem('carrot', CARROT_COUNT, 'img/carrot.png');
   addItem('bug', BUG_COUNT, 'img/bug.png');
 }
@@ -115,16 +132,26 @@ function onFieldClick(event) {
   }
   const target = event.target;
   if (target.matches('.carrot')) {
-    //당근
+    // 당근!!
     target.remove();
     score++;
+    playSound(carrotSound);
+    updateScoreBoard();
     if (score === CARROT_COUNT) {
       finishGame(true);
     }
   } else if (target.matches('.bug')) {
-    stopGameTimer();
     finishGame(false);
   }
+}
+
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
+}
+
+function stopSound(sound) {
+  sound.pause();
 }
 
 function updateScoreBoard() {
